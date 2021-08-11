ead6f1000095a716a5dcc018dc50287c2d83bb25
Mateus Fonseca <mateus.xfl@gmail.com>
2021-08-11 16:15:46 -0300

    correction in the comments

--- trabalho.py
+++ trabalho.py
@@ -18,15 +18,13 @@ def heapify(numeros, totalDeNumerosNaPilha, posicaoRaiz):
     # Mude a raiz, se necessário ( entra aqui caso tenha entrado em uma das duas condições anteriores ).
     if posicaoRaizAuxiliar != posicaoRaiz:
         numeros[posicaoRaiz], numeros[posicaoRaizAuxiliar] = numeros[posicaoRaizAuxiliar], numeros[posicaoRaiz]  # Faz a troca da raiz pelo maior de seus filhos.
- 
-        # Empilhe a raiz novamente ( agora com o valor filho que mudou de posição ).
-        heapify(numeros, totalDeNumerosNaPilha, posicaoRaizAuxiliar)
+        heapify(numeros, totalDeNumerosNaPilha, posicaoRaizAuxiliar) # Empilhe a raiz novamente ( agora com o valor da antiga raiz, só que em sua nova posição ).
 
 numeros = list() # Array que irei preencher com os números a serem ordenados.
 
 entrada = open('entrada.txt','r') # Lê uma entrada TXT que contém o total de números na linha 1, e nas demais linhas: os números a serem ordenados.
 
-totalDeNumerosNaPilha = int(entrada.readlines(1)[0]) # Total de números a serem ordenados.
+totalDeNumerosNaPilha = int(entrada.readlines(1)[0]) # Total de números a serem ordenados ( contido na linha 1 ).
 
 for linha in entrada: # Preenche o vetor de números a serem ordenados ( cria o maxheap ).
     numeros.append(int(linha))
@@ -41,14 +39,14 @@ for i in range(totalDeNumerosNaPilha//2 - 1, -1, -1):
 
 # Um por um extrai um elemento da pilha começando do ultimo elemento do maxheap.
 for i in range(totalDeNumerosNaPilha-1, 0, -1):
-    numeros[i], numeros[0] = numeros[0], numeros[i]  # Faz a troca.
+    numeros[i], numeros[0] = numeros[0], numeros[i]  # Faz a troca, botando o maior no final.
     heapify(numeros, i, 0) # Heapifica a árvore novamente partindo da raiz ( Passo i como totalDeNumeros na pilha pra nao trocar pelos que já estão corretos no final).
 
-# print(numeros) # Números já ordenados ---------------------------------------- 
+# print(numeros) # Números já ordenados ----------------------------------------
 
 arquivo = open("saida.txt", "w") # Escreve a saída (resposta).
 
-linhas = list()
+linhas = list() # Inicializa o vetor de linhas, para no fim adicioná-lo ao arquivo TXT de saída.
 
 linhas.append(str(totalDeNumerosNaPilha) + '\n') # Adiciona o total de números na primeira linha, assim como é solicitado o retorno.
 print(str(totalDeNumerosNaPilha))
